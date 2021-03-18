ae46daf22ae38c7e9ccd3b91d401384d769237cb
Mingyu3 <aeneascon@naver.com>
2021-03-19 00:26:23 +0900

    commit 14.7

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,88 +1,89 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const categories = [
-	{
-		name: 'All',
-		text: '전체보기',
-	},
-	{
-		name: 'Business',
-		text: '비즈니스',
-	},
-	{
-		name: 'Entertainment',
-		text: '엔터테인먼트',
-	},
-	{
-		name: 'Health',
-		text: '건강',
-	},
-	{
-		name: 'Science',
-		text: '과학',
-	},
-	{
-		name: 'Sports',
-		text: '스포츠',
-	},
-	{
-		name: 'Technology',
-		text: '기술',
-	},
+  {
+    name: 'All',
+    text: '전체보기',
+  },
+  {
+    name: 'Business',
+    text: '비즈니스',
+  },
+  {
+    name: 'Entertainment',
+    text: '엔터테인먼트',
+  },
+  {
+    name: 'Health',
+    text: '건강',
+  },
+  {
+    name: 'Science',
+    text: '과학',
+  },
+  {
+    name: 'Sports',
+    text: '스포츠',
+  },
+  {
+    name: 'Technology',
+    text: '기술',
+  },
 ];
 
 const CategoriesBlock = styled.div`
-	display: flex;
-	padding: 1rem;
-	width: 768px;
-	margin: 0 auto;
-	@media screen and (max-width: 768px) {
-		width: 100%;
-		overflow-x: auto;
-	}
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  margin: 0 auto;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    overflow-x: auto;
+  }
 `;
-const Category = styled.div`
-	font-size: 1.125rem;
-	cursor: pointer;
-	white-space: pre;
-	text-decoration: none;
-	color: inherit;
-	padding-bottom: 0.25rem;
+const Category = styled(NavLink)`
+  font-size: 1.125rem;
+  cursor: pointer;
+  white-space: pre;
+  text-decoration: none;
+  color: inherit;
+  padding-bottom: 0.25rem;
 
-	&:hover {
-		color: #495057;
-	}
+  &:hover {
+    color: #495057;
+  }
 
-	${(props) =>
-		props.active &&
-		css`
-			font-weight: 600;
-			border-bottom: 2px solid #22b8cf;
-			color: #22b8cf;
-			&:hover {
-				color: #3bc9db;
-			}
-		`}
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &:hover {
+      color: #3bc9db;
+    }
+  }
 
-	& + & {
-		margin-left: 1rem;
-	}
+  & + & {
+    margin-left: 1rem;
+  }
 `;
-const Categories = ({ category, onSelect }) => {
-	return (
-		<CategoriesBlock>
-			{categories.map((c) => (
-				<Category
-					key={c.name}
-					active={category === c.name}
-					onClick={() => onSelect(c.name)}
-				>
-					{c.name}
-				</Category>
-			))}
-		</CategoriesBlock>
-	);
+
+const Categories = () => {
+  return (
+    <CategoriesBlock>
+      {categories.map((c) => (
+        <Category
+          key={c.name}
+          activeClassName="active"
+          exact={c.name === 'All'}
+          to={c.name === 'All' ? '/' : `/${c.name}`}
+        >
+          {c.name}
+        </Category>
+      ))}
+    </CategoriesBlock>
+  );
 };
 
 export default Categories;
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -4,54 +4,54 @@ import axios from '../../node_modules/axios/index';
 import NewsItem from './NewsItem';
 
 const NewsListBlock = styled.div`
-	box-sizing: border-box;
-	padding-bottom: 3rem;
-	width: 768px;
-	margin: 0 auto;
-	margin-top: 2rem;
-	@media screen and (max-width: 768px) {
-		width: 100%;
-		padding-left: 1rem;
-		padding-right: 1rem;
-	}
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
 `;
 
 const NewsList = ({ category }) => {
-	const [articles, setArticles] = useState(null);
-	const [loading, setLoading] = useState(null);
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			setLoading(true);
-			try {
-				const query = category === 'All' ? '' : `&category=${category}`;
-				const response = await axios.get(
-					`http://newsapi.org/v2/top-headlines?country=us${query}&apiKey=d7203d3735dc456d9ab93874c4a334dd`,
-				);
-				setArticles(response.data.articles);
-			} catch (e) {
-				console.log(e);
-			}
-			setLoading(false);
-		};
-		fetchData();
-	}, [category]);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === 'All' ? '' : `&category=${category}`;
+        const response = await axios.get(
+          `http://newsapi.org/v2/top-headlines?country=us${query}&apiKey=d7203d3735dc456d9ab93874c4a334dd`,
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [category]);
 
-	if (loading) {
-		return <NewsListBlock>Processing...</NewsListBlock>;
-	}
+  if (loading) {
+    return <NewsListBlock>Processing...</NewsListBlock>;
+  }
 
-	if (!articles) {
-		return null;
-	}
+  if (!articles) {
+    return null;
+  }
 
-	return (
-		<NewsListBlock>
-			{articles.map((article) => (
-				<NewsItem key={article.url} article={article}></NewsItem>
-			))}
-		</NewsListBlock>
-	);
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article}></NewsItem>
+      ))}
+    </NewsListBlock>
+  );
 };
 
 export default NewsList;
